d1157cadb0cd6a7afbe9c419cce9fd3f523aa53e
Ivan Delgado <46804151+vndlgd@users.noreply.github.com>
2023-06-28 00:07:51 -0700

    Implement buttons for user to select Rock Paper or Scissors and display winner on the page

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const choices = ["rock", "paper", "scissors"];
 // initial score of users at start of game
 let userScore = 0;
 let computerScore = 0;
+let playerSelection = "";
+let computerSelection = "";
 
 // retrieve a random choice in choices list to assign to computerSelection
 function getComputerChoice() {
@@ -27,45 +29,46 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function getRoundWinner(playerSelection, computerSelection) {
+    let winner = ""
     // make user input case insensitive
     playerSelection = playerSelection.toLowerCase();
 
     // if user chooses same choice as computer 
     if (playerSelection === computerSelection) {
-        results = "It's a Tie!";
+        winner = "It's a Tie!";
     }
 
     // if user chooses rock
     else if (playerSelection === "rock") {
         if (computerSelection === "paper") {
-            results = "You Lose! Paper beats Rock";
+            winner = "You Lose! Paper beats Rock";
         }
         else if (computerSelection === "scissors") {
-            results = "You Win! Rock beats Paper";
+            winner = "You Win! Rock beats Paper";
         }
     }
 
     // if user chooses paper
     else if (playerSelection === "paper") {
         if (computerSelection === "rock") {
-            results = "You Win! Paper beats Rock";
+            winner = "You Win! Paper beats Rock";
         }
         else if (computerSelection === "scissors") {
-            results = "You Lose! Scissors beats Paper";
+            winner = "You Lose! Scissors beats Paper";
         }
     }
 
     // if user chooses scissors
     else if (playerSelection === "scissors") {
         if (computerSelection === "rock") {
-            results = "You Lose! Rock beats Scissors";
+            winner = "You Lose! Rock beats Scissors";
         }
         else if (computerSelection === "paper") {
-            results = "You Win! Scissors beats Paper";
+            winner = "You Win! Scissors beats Paper";
         }
     }
 
-    return results;
+    return winner;
 }
 
 function updateScores(result) {
@@ -92,12 +95,42 @@ function printGameWinner() {
 
 
 function game() {
-    let playerSelection = prompt("Enter your choice (rock, paper, scissors):");
-    let computerSelection = GetComputerChoice();
-    let result = playRound(playerSelection, computerSelection);
-    console.log(result);
+    let result = "";
 
-    printGameWinner();
+    // let playerSelection = prompt("Enter your choice (rock, paper, scissors):");
+    const buttons = document.querySelector('#buttons');
+    const rock = document.createElement('button');
+    const paper = document.createElement('button');
+    const scissors = document.createElement('button');
+
+    // place text inside of each button
+    rock.textContent = 'Rock';
+    paper.textContent = 'Paper';
+    scissors.textContent = 'Scissors';
+
+    // place element into the DOM 
+    buttons.appendChild(rock);
+    buttons.appendChild(paper);
+    buttons.appendChild(scissors);
+
+    const displayResult = document.querySelector('#displayResult');
+    const winner = document.createElement('div');
+
+    // when button is clicked, playerSelection equals value inside of button 
+    document.getElementById('buttons').addEventListener("click", function (e) {
+        playerSelection = e.target.textContent;
+        console.log(playerSelection);
+        computerSelection = getComputerChoice();
+        result = playRound(playerSelection, computerSelection);
+        // display results on page
+        winner.textContent = result;
+        displayResult.appendChild(winner);
+        // console.log(result);
+    });
+
+
+
+    // printGameWinner();
 }
 
 // play game 
